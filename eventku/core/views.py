5b100372b0fe81245645c7c3a32e53e85a144bc8
daffabilnadzary1 <daffabilnadzary1@gmail.com>
2022-07-08 18:59:33 +0700

    implement flask-mail API

--- eventku/core/views.py
+++ eventku/core/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from .models import Event
+from .mailapp.app import send_group_invitation
+from .forms import emailForm
 
 # Create your views here.
 
@@ -26,7 +28,20 @@ class eventDetail(View):
         except Event.DoesNotExist:
             return redirect('core:login')
 
-
+    def post(self, request, event_id, *args, **kwargs):
+        event = Event.objects.get(uuid = event_id)
+        if request.method == "POST":
+          # Get the posted form
+            email_form = emailForm(request.POST)
+            
+            if email_form.is_valid():
+                # do anything here
+                email = email_form.cleaned_data['email']
+                print(email)
+                send_group_invitation()
+                return render(request, 'eventDetail.html', {
+                    'event': event
+                })
 
 class paymentSuccess(View):
     def get(self, request, *args, **kwargs):
