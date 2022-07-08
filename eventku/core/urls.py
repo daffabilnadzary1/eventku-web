2e09ee5cc0d48a1a88d1f360597f9d261ca1c4de
daffabilnadzary1 <daffabilnadzary1@gmail.com>
2022-07-08 10:09:13 +0700

    fixing login.html

--- eventku/core/urls.py
+++ eventku/core/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', Home.as_view()),
     path('detail/', eventDetail.as_view(), name = 'event_detail'),
     path('payment_success/', paymentSuccess.as_view(), name = 'payment_success'),
-    path('login/', login.as_view(), name = "login"),
 ]
