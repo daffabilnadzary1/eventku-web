e496edcffbdf250d2d4e33f39f6898e5f2353961
daffabilnadzary1 <daffabilnadzary1@gmail.com>
2022-07-08 13:44:00 +0700

    fetching events from sqlite works

--- eventku/core/views.py
+++ eventku/core/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from .models import Event
 
 # Create your views here.
 
+@method_decorator(login_required,name='dispatch')
 class Home(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+        events = Event.objects.all()
+
+        return render(request, 'index.html', {
+            'events':events
+        })
 
 class eventDetail(View):
     def get(self, request,*args, **kwargs):
